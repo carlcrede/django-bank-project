2563e0a256943dcd38d196f6c03df12782e26b8d
Alex <a.sandrovschii@gmail.com>
2022-10-01 17:50:05 +0200

    added urls in comments

--- bank_project/bank_app/urls.py
+++ bank_project/bank_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'bank_app'
 
 urlpatterns = [
-   path('index/', views.index, name='index'),
-   path('create_employee/', views.create_employee, name='create_employee'),
-   path('create_customer/', views.create_customer, name='create_customer'),
+   path('index/', views.index, name='index'), # to access it: localhost:8000/bank/index/
+   path('create_employee/', views.create_employee, name='create_employee'), # to access it: localhost:8000/bank/create_employee
+   path('create_customer/', views.create_customer, name='create_customer'), # to access it: localhost:8000/bank/create_customer
 ]
