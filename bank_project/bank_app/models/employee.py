7fbbde6ffc5df136be65c445643da510ad572f01
Alex <a.sandrovschii@gmail.com>
2022-10-01 17:48:24 +0200

    code to create customer

--- bank_project/bank_app/models/employee.py
+++ bank_project/bank_app/models/employee.py
@@ -1,17 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from .customer import Customer
 
 class Employee(models.Model):
     employee = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    def create_customer(self, *data):
-        pass
-
-    def create_account(self, *data):
-        pass
-
-    # def __str__(self):
-    #         return self.employee
 
     @classmethod
     def create_employee(self, fname, lname, email, uname, passwd):
@@ -20,3 +12,17 @@ class Employee(models.Model):
         new_empoyee = Employee.objects.create(employee=new_user)
         # print(f"new_empoyee {new_empoyee}")
         return new_empoyee
+
+    @classmethod  
+    def create_customer(self, fname, lname, email, uname, phone, rank, passwd):
+        new_user = User.objects.create_user(first_name=fname, last_name=lname, email=email, username=uname, password=passwd)
+        # print(f"user: {user}")
+        new_customer = Customer.objects.create(user=new_user, phone=phone, rank=rank)
+        # print(f"new_empoyee {new_empoyee}")
+        return new_customer
+
+    def create_account(self, *data):
+        pass
+
+    # def __str__(self):
+    #         return self.employee
--- bank_project/bank_app/views.py
+++ bank_project/bank_app/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, reverse
 from django.http import HttpResponseRedirect
 from .models import Employee
+from .models import Customer
+
+def index(request):
+    return render(request, 'bank_app/index.html', {})
 
 def create_employee(request):
-   context = {}
-   if request.method == "POST":
-      password = request.POST['password']
-      confirm_password = request.POST['confirm_password']
-      user_name = request.POST['user_name']
-      first_name = request.POST['first_name']
-      last_name = request.POST['last_name']
-      email = request.POST['email']
-      if password == confirm_password:
+    context = {}
+    if request.method == "POST":
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        user_name = request.POST['user_name']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        if password == confirm_password:
             Employee.create_employee(first_name, last_name, email, user_name, password)
             # if :
             return HttpResponseRedirect(reverse('bank_app:index'))
@@ -19,14 +23,33 @@ def create_employee(request):
             #    context = {
             #       'error': 'Could not create user account - please try again.'
             #    }
-      else:
+        else:
             context = {
-               'error': 'Passwords did not match. Please try again.'
+                'error': 'Passwords did not match. Please try again.'
             }
-   return render(request, 'bank_app/create_employee.html', context)
-
-def index(request):
-   return render(request, 'bank_app/index.html', {})
+    return render(request, 'bank_app/create_employee.html', context)
 
 def create_customer(request):
-   ...
+    context = {}
+    if request.method == "POST":
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        user_name = request.POST['user_name']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        rank = request.POST['rank']
+        if password == confirm_password:
+            Employee.create_customer(first_name, last_name, email, user_name, phone, rank, password)
+            # if :
+            return HttpResponseRedirect(reverse('bank_app:index'))
+            # else:
+            #    context = {
+            #       'error': 'Could not create user account - please try again.'
+            #    }
+        else:
+            context = {
+                'error': 'Passwords did not match. Please try again.'
+            }
+    return render(request, 'bank_app/create_customer.html', context)
