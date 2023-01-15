07e557fac1f180a05873af257a8d8b47b3f7f1eb
Linux User <carl2@kea-dev.members.linode.com>
2023-01-15 19:39:45 +0100

    entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,6 @@ python manage.py check --deploy
 python manage.py collectstatic --noinput
 python manage.py migrate --no-input
 #python manage.py seed_data
+python manage.py rqworker --with-scheduler &
+python manage.py crontab add &
 gunicorn bank_project.asgi:application -b 0.0.0.0:8000 -k uvicorn.workers.UvicornWorker
